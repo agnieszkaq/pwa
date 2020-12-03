93e7de12d856bed636d2ac1102c03deaa8e77a28
Aga K <59511312+agnieszkaq@users.noreply.github.com>
2020-12-03 14:05:19 +0100

    finish

---
+++ index.js
@@ -0,0 +1,18 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/serviceWorker.js');
+}
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    addBtn.style.display = 'block';
+
+    addBtn.addEventListener('click', (e) => {
+        addBtn.style.display = 'none';
+    });
+});
