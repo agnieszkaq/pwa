743615ee3836ea6648fbf3a6df2ce010ab522997
Aga K <59511312+agnieszkaq@users.noreply.github.com>
2020-12-04 12:28:37 +0100

    check github page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ if ('serviceWorker' in navigator) {
         .register('/serviceWorker.js');
 }
 
+
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
@@ -12,7 +13,29 @@ window.addEventListener('beforeinstallprompt', (e) => {
     deferredPrompt = e;
     addBtn.style.display = 'block';
 
-    addBtn.addEventListener('click', (e) => {
-        addBtn.style.display = 'none';
-    });
+});
+
+const addBtn1 = document.querySelector('.add-button1');
+
+addBtn1.addEventListener('click', (e) => {
+    if (!("Notification" in window)) {
+        alert("Ta przeglądarka nie obsługuje powiadomień");
+    }
+
+    // Sprawdźmy czy uprawnienia dla powiadomienia zostały nadane
+    else if (Notification.permission === "granted") {
+        // jeżeli są tworzymy powiadomienie
+        var notification = new Notification("Hello world");
+    }
+
+    // W innym przypadku tworzymy zapytanie o uprawnienia
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function(permission) {
+            //Jeżeli użytkownik zaakceptuje tworzymy powiadomienie
+            if (permission === "granted") {
+                var notification = new Notification("Hello wrold");
+            }
+        });
+    }
+
 });
